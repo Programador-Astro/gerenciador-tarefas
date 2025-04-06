65c1e5c33099fbccfffc076264d345b9e25503ed
Astro <programadorastro@gmail.com>
2025-04-06 12:43:39 -0300

    Adiciona gunicorn para deploy

--- run.py
+++ run.py
@@ -1,8 +1,9 @@
 from app import create_app, db
 from flask_migrate import Migrate
-
+import os
 app = create_app()
 migrate = Migrate(app, db)  # ← ESSA LINHA É ESSENCIAL!
 
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=os.getenv("FLASK_DEBUG", "false").lower() == "true")
